902a16476cd7c2b364dd826c48594d7ea697e900
nms377 <nms377@nyu.edu>
2016-11-30 22:25:22 -1000

    Changed button 9 using fat arrow function and using the call method passing three parameters.

--- js/app.js
+++ js/app.js
@@ -185,6 +185,9 @@ btn8.addEventListener('click', function(event){
    *   and passing two aditional arguments, event and 'light-green'
    */
 
+btn9.addEventListener('click', (event) =>{
+  setMyLightClass.call(btn9, event, 'light-green');
+});
 
   /*
    * Declare a new const named setLight10Green
